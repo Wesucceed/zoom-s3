de024374b252af6e401cb49560f6bbd40f502e52
Jephthah Kwame Mensa <jkm255@cornell.edu>
2023-06-06 12:24:36 -0400

    auth and access token

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # zoom-s3
 
-##App to get lists of zoom recordings from a zoom account and upload the records to s3 bucket
+## App to get lists of zoom recordings from a zoom account and upload the records to s3 bucket
 
 Process: https://docs.google.com/document/d/1HwWxz_pKyAs8YOuS8RqJh8TZ97J8gwdRv-uXBut-Kzg/edit?usp=sharing
 
--- app.py
+++ app.py
@@ -1,11 +1,42 @@
+import requests
 
 
+SERVER_URL = "https://api.zoom.us/v2"
+LIST_OF_RECORDINGS_URL = "/users/{userId}/recordings"
 
 
+AUTH_URL = "https://zoom.us/oauth/authorize"
+auth_parameters = {
+    "response_type" :,
+    "redirect_uri" :,
+    "client_id" : 
+}
+auth_header = {
+    "Authorization",
+    "Content-Type"
+}
+
+
+ACCESS_TOKEN_URL = "https://zoom.us/oauth/token"
+access_url = {
+    "code":,
+    "grant_type"
+}
 
 def get_all_recordings(userId):
     """
     Gets all recordings from the user account
     
     Parameter userId: is an integer representing the user id
+    """
+
+
+def get_auth_code():
+    """
+    Returns the authorization code of the user
+    """
+
+def get_access_token():
+    """
+    Returns the access token of the user
     """
